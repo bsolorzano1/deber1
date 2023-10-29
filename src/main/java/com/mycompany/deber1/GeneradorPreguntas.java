997df9e8fd199562822db32a3f4fab1eb8d93a03
Belen Solorzano <bsolorzano8372@utm.edu.ec>
2023-10-28 20:53:16 -0500

    Commit 2 generador de preguntas

--- src/main/java/com/mycompany/deber1/GeneradorPreguntas.java
+++ src/main/java/com/mycompany/deber1/GeneradorPreguntas.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.deber1;
+import java.security.SecureRandom;
 
-/**
- *
- * @author LENOVO
- */
 public class GeneradorPreguntas {
-    
+
+    private SecureRandom randomGen;
+    private int difficulty;
+    private int problemType;
+
+    public GeneradorPreguntas(int difficulty, int problemType) {
+        this.randomGen = new SecureRandom();
+        this.difficulty = difficulty;
+        this.problemType = problemType;
+    }
+
+    public int[] generarOperandos() {
+        int maxNum = (int) Math.pow(10, difficulty) - 1;
+        return new int[] { randomGen.nextInt(maxNum) + 1, randomGen.nextInt(maxNum) + 1 };
+    }
+
+    public String generarPregunta(int num1, int num2) {
+        String opSymbol = (problemType == 1) ? "+" : (problemType == 2) ? "-" : (problemType == 3) ? "*" : "/";
+        return "Resuelve " + num1 + " " + opSymbol + " " + num2;
+    }
+
+    public double calcularRespuesta(int num1, int num2) {
+        return (problemType == 1) ? num1 + num2 : (problemType == 2) ? num1 - num2 : (problemType == 3) ? num1 * num2 : (double) num1 / num2;
+    }
 }
